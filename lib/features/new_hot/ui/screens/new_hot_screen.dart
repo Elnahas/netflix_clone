9fc598e539406a251e723470576a7cb72764de45
Hazem <hazemelnahas2010@gmail.com>
2024-06-27 21:38:19 +0300

    - add empty search screen - add empty new & hot screen - finished setup nav bar

---
+++ lib/features/new_hot/ui/screens/new_hot_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewHotScreen extends StatelessWidget {
+  const NewHotScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
