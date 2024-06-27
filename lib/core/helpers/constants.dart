37b28566d2da52e369f5b12f82e87925239e9873
Hazem <hazemelnahas2010@gmail.com>
2024-06-27 23:30:05 +0300

    - add spacing file to can make vertical or horizontal space - end AppBar ui for home screen

--- lib/core/helpers/constants.dart
+++ lib/core/helpers/constants.dart
@@ -3,4 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Constants {
   static  String userToken =dotenv.get("API_TOKEN" , fallback: "" );
+  static const String baseImageUrl = "https://image.tmdb.org/t/p/w500/";
+  static const String baseUrl = "https://api.themoviedb.org/3";
 }
--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:netflix_clone/core/helpers/spacing.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.asset(
+          'assets/icons/logo.png',
+          height: 50.h,
+          width: 120.w,
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+
+          CircleAvatar(
+            radius: 20.r,
+            backgroundImage: const NetworkImage(
+                'https://media.licdn.com/dms/image/D4D03AQFmicEnnQAowg/profile-displayphoto-shrink_800_800/0/1719037982898?e=1724889600&v=beta&t=_4cazPwgUc51_HgWwWFxaeglKEDNJGhks59D_WZWHzA'),
+          ),
+          horizontalSpace(15)
+          
+        ],
+      ),
     );
   }
 }
