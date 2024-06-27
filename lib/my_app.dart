eb3d0f06c54f53bb1693168a5f713b43b32ee7a8
Hazem <hazemelnahas2010@gmail.com>
2024-06-27 13:01:14 +0300

    - implement ScreenUtil - change them mode to dark

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Netflix Clone',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        title: 'Netflix Clone',
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(
+          textTheme: TextTheme(
+              bodyLarge: TextStyle(color: Colors.white, fontSize: 24.sp)),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
+              .copyWith(surface: Colors.black),
+          useMaterial3: true,
+        ),
+        home: Container(),
       ),
-      home: Container(),
     );
   }
 }
