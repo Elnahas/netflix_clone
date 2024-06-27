a12afcbb9e7dbcc0e2b157fc62c3d11f22ed6234
Hazem <hazemelnahas2010@gmail.com>
2024-06-27 15:32:10 +0300

    - handel splash screen and set Lottie - make AnimationController to control after finish animation Navigator to home Screen

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'features/splash/ui/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,13 +18,16 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.dark,
         theme: ThemeData(
-          textTheme: TextTheme(
-              bodyLarge: TextStyle(color: Colors.white, fontSize: 24.sp)),
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(color: Colors.white, fontSize: 24),
+            bodyMedium: TextStyle(color: Colors.white, fontSize: 20),
+          ),
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
               .copyWith(surface: Colors.black),
           useMaterial3: true,
+          fontFamily: GoogleFonts.ptSans().fontFamily,
         ),
-        home: Container(),
+        home: const SplashScreen(),
       ),
     );
   }
