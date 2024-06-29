5f44a77196d021b457c79427266cbc8805078bb5
Hazem <hazemelnahas2010@gmail.com>
2024-06-29 11:44:41 +0300

    - add Api Constants file and store end point , base url , user token and Api Errors

--- lib/core/helpers/constants.dart
+++ lib/core/helpers/constants.dart
@@ -1,8 +1,5 @@
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Constants {
-  static  String userToken =dotenv.get("API_TOKEN" , fallback: "" );
-  static const String baseImageUrl = "https://image.tmdb.org/t/p/w500/";
-  static const String baseUrl = "https://api.themoviedb.org/3";
+
 }
---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiConstants {
+  static final String userToken = dotenv.get("API_TOKEN", fallback: "");
+  static const String apiBaseUrl = "https://image.tmdb.org/t/p/w500/";
+  static const String baseUrl = "https://api.themoviedb.org/3/";
+  static const String movieUpcoming = "movie/upcoming";
+}
+
+class ApiErrors {
+  static const String badRequestError = "badRequestError";
+  static const String noContent = "noContent";
+  static const String forbiddenError = "forbiddenError";
+  static const String unauthorizedError = "unauthorizedError";
+  static const String notFoundError = "notFoundError";
+  static const String conflictError = "conflictError";
+  static const String internalServerError = "internalServerError";
+  static const String unknownError = "unknownError";
+  static const String timeoutError = "timeoutError";
+  static const String defaultError = "defaultError";
+  static const String cacheError = "cacheError";
+  static const String noInternetError = "noInternetError";
+  static const String loadingMessage = "loading_message";
+  static const String retryAgainMessage = "retry_again_message";
+  static const String ok = "Ok";
+}
